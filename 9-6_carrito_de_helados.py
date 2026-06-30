e88330fb3537112fcc9c449434f71ae52a306e21
dalejandrohurtadop <dahurtadop1@gmail.com>
2026-06-30 06:02:01 -0500

    Add new python files cap 9 class

--- 9-6_carrito_de_helados.py
+++ 9-6_carrito_de_helados.py
@@ -5,11 +5,11 @@ class Restaurante:
         self.numero_servicio = numero_servicio
 
     def describir_restaurante(self):
-        describ_resta = f'El {self.nombre_restaurate.title()} es un restaurante de tipo {self.tipo_cocina.title()}'
+        describ_resta = f"El {self.nombre_restaurate.title()} es un restaurante de tipo {self.tipo_cocina.title()}"
         return describ_resta
 
     def abrir_restaurante(self):
-        mensaje = f'Buen dìa, el restaurante {self.nombre_restaurate.title()} le da la bienvenida'
+        mensaje = f"Buen dìa, el restaurante {self.nombre_restaurate.title()} le da la bienvenida"
         return mensaje
 
     def establecer_numero_servicio(self, num_servicio_inicial):
@@ -22,14 +22,14 @@ class Restaurante:
 class CarritoDeHelados(Restaurante):
     def __init__(self, nombre_restaurate, tipo_cocina, numero_servicio):
         super().__init__(nombre_restaurate, tipo_cocina, numero_servicio)
-        self.sabores = ['Arequipe', 'Mora', 'Vainilla', 'Chocolate', 'Mandarina']
+        self.sabores = ["Arequipe", "Mora", "Vainilla", "Chocolate", "Mandarina"]
 
     def elegir_sabor(self):
-        print('Los sabores de helados disponibles son: ')
+        print("Los sabores de helados disponibles son: ")
         for i, sabor in enumerate(self.sabores):
-            print(f'{i+1}. {sabor}')
+            print(f"{i + 1}. {sabor}")
 
 
-mi_carrito_helados = CarritoDeHelados('Mi carrito', 'Helados', 0)
+mi_carrito_helados = CarritoDeHelados("Mi carrito", "Helados", 0)
 
 mi_carrito_helados.elegir_sabor()
--- 9-9_mejora_battery.py
+++ 9-9_mejora_battery.py
@@ -1,6 +1,6 @@
 class Car:
-    """Un intento de representar un coche
-    """
+    """Un intento de representar un coche"""
+
     def __init__(self, make, model, year):
         """Incicializa atributos para describir coche
         Args:
@@ -14,24 +14,21 @@ class Car:
         self.odometer_reding = 0
 
     def get_descriptive_name(self):
-        """Devuelve el nombre descriptivo del coche
-        """
-        long_name = f'{self.make} {self.model} {self.year}'
+        """Devuelve el nombre descriptivo del coche"""
+        long_name = f"{self.make} {self.model} {self.year}"
         return long_name.title()
-    
+
     def read_odometer(self):
-        """Imprime una declaraciòn mostrando el kilometraje del vehiculo
-        """
-        print(f'This car has {self.odometer_reding} miles on it')
+        """Imprime una declaraciòn mostrando el kilometraje del vehiculo"""
+        print(f"This car has {self.odometer_reding} miles on it")
 
     def update_odometer(self, mileage):
-        """Configura la lectura del cuentakilometros para el valor dado.
-        """
+        """Configura la lectura del cuentakilometros para el valor dado."""
         if mileage >= self.odometer_reding:
             self.odometer_reding = mileage
         else:
             print("You can't roll back an odometer!")
-        
+
     def increment_odometer(self, miles):
         """Incrementa el cuenta kilometros un valor dado
 
@@ -40,9 +37,10 @@ class Car:
         """
         self.odometer_reding += miles
 
+
 class Battery:
-    """Un intento de modelar una bateria de un coche eléctrico
-    """
+    """Un intento de modelar una bateria de un coche eléctrico"""
+
     def __init__(self, batery_size=40):
         """Inicializando atributos de la bateria
 
@@ -52,23 +50,20 @@ class Battery:
         self.batery_size = batery_size
 
     def describe_battery(self):
-        """Imprime una declaración que describe el tamaño de la batería
-        """
-        print(f'This car has a {self.batery_size}-kWh battery.')
+        """Imprime una declaración que describe el tamaño de la batería"""
+        print(f"This car has a {self.batery_size}-kWh battery.")
 
     def get_range(self):
-        """Imprime una frase sobre la autonomia que ofrece la batería
-        """
+        """Imprime una frase sobre la autonomia que ofrece la batería"""
         if self.batery_size == 40:
-            range =150
+            range = 150
         elif self.batery_size == 65:
             range = 225
-        
-        print(f'This car can go about {range} milles on a full charge')
+
+        print(f"This car can go about {range} milles on a full charge")
 
     def upgrade_battery(self):
-        """Actualiza la capacidad de la bateria a 65 kWh
-        """
+        """Actualiza la capacidad de la bateria a 65 kWh"""
         if self.batery_size < 65:
             self.batery_size = 65
 
@@ -92,7 +87,7 @@ class ElectricCar(Car):
         self.battery = Battery()
 
 
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
+my_leaf = ElectricCar("nissan", "leaf", 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
 my_leaf.battery.get_range()
